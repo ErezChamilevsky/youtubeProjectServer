c0f776912695314b8143f6449511dc21175b43b6
Izhak Ben David <izhak573@gmail.com>
2024-06-24 01:03:31 +0300

    creating the basic directory hierarchy

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+var server = express();
+
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.urlencoded({ extended: true}));
+server.use(express.json);
+
+const cors = require('cors');
+server.use(cors());
+
+const customEnv = require('custom-env');
+customEnv.env(process.env.NODE_ENV, './config');
+console.log(process.env.CONNECTION_STRING);
+console.log(process.env.PORT);
+
+
+const mongoose = require('mongoose');
+//connect to MongoDB
+mongoose.connect(process.env.CONNECTION_STRING, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+ 
+
+
+server.use(express.static('public'));
+
+
+server.listen(process.env.PORT); //server listen in PORT (define in config/.env.local).

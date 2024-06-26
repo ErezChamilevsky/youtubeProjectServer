6897379ec07f3867a4da97024644d54c8c75df22
Izhak Ben David <izhak573@gmail.com>
2024-06-26 16:34:12 +0300

    fix typo in register

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,7 @@
 const registerService = require('../services/register');
 
 const createUser = async (req, res) => {
-    result = await registerService.craeteUser(req.body.userName, req.body.userPassword, req.body.userConfirmPassword, req.body.displayName, req.body.userImgFile)
+    result = await registerService.createUser(req.body.userName, req.body.userPassword, req.body.userConfirmPassword, req.body.displayName, req.body.userImgFile)
     
     if (result.success) {
         res.status(200).json({ message: result.message, user: result.user });
--- services/register.js
+++ services/register.js
@@ -4,36 +4,13 @@
 const User = require('../models/user');
 userIdCounter = 1;
 
-const createUser = async (userName, userPassword, userConfirmPassword, displayName, userImgFile) => {
-    // Check if the userName already exists
-    userNameExist = checkIfUserameExist(userName);
-    if(!userNameExist)
-        return { success: false, message: 'Username already exists' };
-
-    //check if the password valid.
-    validPassword = validatePassword(userPassword);
-    if(!validPassword)
-        return { success: false, message: 'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number.'};
-    
-    //check if the password and confirm password are same
-    confirmPassword = checkIfConfirmPassSameToPass(userPassword, userConfirmPassword)
-    if(!confirmPassword)
-        return { success: false, message: 'Password and Confirm Password fields do not match.' };
-
-    const newUser = new User({userId : userIdCounter, userName : userName , userPassword : userPassword, displayName : displayName, userImgFile : userImgFile});
-    userIdCounter++; //increment the Id counter
-    await newUser.save(); //save the new user in mongoDB ,and return the new User object and not the promise because 'await'
-    return { success: true, message: 'User created successfully', user: newUser }; // Return the new user object
-    
-}
-
-
 const checkIfUserNameExist = async(userName) =>{
     const existingUser = await User.findOne({ userName });
     if (existingUser) {
-       return null;
+       return true;
     }
-    return 1; //if userName doesnt exist yet.
+    else{ return false;} //if userName doesnt exist yet.
+  
 }
 
 //function who validate password
@@ -44,6 +21,30 @@ const validatePassword = (password) => {
 
 const checkIfConfirmPassSameToPass = (userPassword, userConfirmPassword) => {
     return userPassword === userConfirmPassword; //check if strings and the data type are equal
+}
+
+
+const createUser = async (userName, userPassword, userConfirmPassword, displayName, userImgFile) => {
+    // Check if the userName already exists
+    const userNameExist = await checkIfUserNameExist(userName);
+    if(userNameExist)
+        return { success: false, message: 'Username already exists' };
+
+    //check if the password valid.
+    const validPassword = validatePassword(userPassword);
+    if(!validPassword)
+        return { success: false, message: 'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number.'};
+    
+    //check if the password and confirm password are same
+    const confirmPassword = checkIfConfirmPassSameToPass(userPassword, userConfirmPassword)
+    if(!confirmPassword)
+        return { success: false, message: 'Password and Confirm Password fields do not match.' };
+
+    const newUser = new User({userId : userIdCounter, userName : userName , userPassword : userPassword, displayName : displayName, userImgFile : userImgFile});
+    userIdCounter++; //increment the Id counter
+    await newUser.save(); //save the new user in mongoDB ,and return the new User object and not the promise because 'await'
+    return { success: true, message: 'User created successfully', user: newUser }; // Return the new user object
+    
 }
 
 module.exports = {createUser,checkIfUserNameExist, validatePassword, checkIfConfirmPassSameToPass};
